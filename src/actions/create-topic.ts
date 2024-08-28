c9b88571e5c97f3459b78c6db14e93b1cd145395
leVert <selmamathis15@gmail.com>
2024-08-28 17:08:56 +0300

    zod validation

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,9 +1,20 @@
 'use server';
+import {z} from 'zod';
+
+const createTopicSchema=z.object({
+    name:z.string().min(3).regex(/[a-z-]/, {message: 'Must be lowercase letters or dashes without spaces'}),
+    description: z.string().min(10),
+});
 
 export async function createTopic(FormData: FormData){
-    const name=FormData.get('name');
-    const description=FormData.get('description');
-    console.log(name, description);
+    const result=createTopicSchema.safeParse({
+        name: FormData.get('name'),
+        description: FormData.get('description')
+    })
+
+    if(!result.success){
+        console.log(result.error.flatten().fieldErrors);
+    }
     //revalidate the homepage
 
 }
