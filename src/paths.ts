bc717d57ea5b6b78c0189538bcd27e3525ec1def
leVert <selmamathis15@gmail.com>
2024-08-28 12:54:30 +0300

    implement path helper

---
+++ src/paths.ts
@@ -0,0 +1,21 @@
+const paths={
+    home(){
+        return "/";
+    },
+
+    topicShow(topicSlug: string){
+        return `/topics/${topicSlug}`;
+    },
+
+    postCreate(topicSlug: string){
+        return  `/topic/${topicSlug}/posts/new`;
+    },
+
+    postShow(topicSlug: string, postId: string){
+        return `/topic/${topicSlug}/posts/${postId}`;
+
+    }
+
+};
+
+export default paths;
