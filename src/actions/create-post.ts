490dd764be11c37e0bf0c5931937930d678084ee
leVert <selmamathis15@gmail.com>
2024-08-30 17:05:53 +0300

    Implement validation: Post creation form

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -1,5 +1,28 @@
 'use server';
-export async function createPost(){
+import type { Post } from "@prisma/client";
+import { z } from "zod";
+const createPostSchema=z.object({
+    title: z.string().min(3),
+    content: z.string().min(10)
+});
+interface CreatePostFormState{
+    errors:{
+        title?:string[]
+        content?:string[],
+        _form?: string[]
+    }
+}
+export async function createPost(formState:CreatePostFormState, FormData:FormData): Promise<CreatePostFormState>{
+    const result =createPostSchema.safeParse({
+        title: FormData.get('title'),
+        content: FormData.get('content')
+    });
+
+    if(!result.success){
+        return{
+            errors: result.error.flatten().fieldErrors
+        }
+    }
     //revalidate topic show page
     
 }
--- src/components/posts/post-create-form.tsx
+++ src/components/posts/post-create-form.tsx
@@ -1,9 +1,12 @@
 'use client'
 
-import { Button, Input, Popover, PopoverContent, PopoverTrigger } from "@nextui-org/react"
+import { Button, Input, Popover, PopoverContent, PopoverTrigger, Textarea } from "@nextui-org/react"
 import FormButton from "../common/form-button"
+import { useFormState } from "react-dom"
+import { createPost } from "@/actions";
 
 export default function PostCreateForm(){
+    const [formState, action]=useFormState(createPost, {errors:{}});
     return (
         <Popover placement="left">
             <PopoverTrigger>
@@ -12,17 +15,21 @@ export default function PostCreateForm(){
                 </Button>
             </PopoverTrigger>
             <PopoverContent>
-                <form>
+                <form action={action}>
                     <div className="flex flex-col gap-4 p-4 w-80">
                         <h3 className="text-lg">Create a Post</h3>
                         <Input
+                            isInvalid={!!formState.errors.title}
+                            errorMessage={formState.errors.title?.join(', ')}
                             name="title"
                             label="Title"
                             labelPlacement="outside"
                             placeholder="Title"
                         />
 
-                        <Input
+                        <Textarea
+                            isInvalid={!!formState.errors.content}
+                            errorMessage={formState.errors.content?.join(', ')}
                             name="content"
                             label="Content"
                             labelPlacement="outside"
